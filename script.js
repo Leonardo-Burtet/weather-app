bdeb50f274cab939c580f0df11d2d8ec627554d0
Leonardo Burtet <leonardoburtet@outlook.com>
2021-08-13 20:45:14 -0300

    refactor js

--- script.js
+++ script.js
@@ -1,62 +1,81 @@
+const dateNow = new Date();
+const day = dayString(dateNow);
+const city = document.querySelector(".city h1");
+const today = document.querySelector(".city h2");
+const degrees = document.querySelector(".degrees");
+const weather = document.querySelector(".weather-app p:last-child");
+const max = document.querySelector(".max p");
+const visibility = document.querySelector(".visibility p");
+const min = document.querySelector(".min p");
+const humidity = document.querySelector(".humidity p");
+const body = document.querySelector("body");
+const info = document.querySelectorAll(".info h3");
+
 if ("geolocation" in navigator) {
   navigator.geolocation.getCurrentPosition(function (position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    getDados(lat, lon);
+    getData(lat, lon);
     //getDados();
   });
 }
 
-async function getDados(lat, lon) {
+async function getData(lat, lon) {
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=decfdd61b5ac2ba68b3b549d378565d0&units=metric`
       //`https://api.openweathermap.org/data/2.5/weather?q=china&appid=decfdd61b5ac2ba68b3b549d378565d0&units=metric&lang=pt_br`
     );
-    const responseJSON = await response.json();
-    console.log(responseJSON);
+    const data = await response.json();
 
-    const city = document.querySelector(".city h1");
-    const today = document.querySelector(".city h2");
-    const date = new Date();
-    console.log(date / 1000);
-    console.log(responseJSON.sys.sunrise);
-    console.log(responseJSON.sys.sunset);
-    city.innerText = `${responseJSON.name}, ${responseJSON.sys.country}`;
-    today.innerText = date;
-    const degrees = document.querySelector(".degrees");
-    degrees.innerText = `${responseJSON.main.temp.toFixed(0)}°`;
+    weatherApp(data);
+  } catch (erro) {
+    console.log(erro);
+  }
+}
 
-    const weather = document.querySelector(".weather-app p:last-child");
-    weather.innerText = responseJSON.weather[0].description;
+function dayString(dateNow) {
+  switch (dateNow.getDay()) {
+    case 1:
+      return "Monday";
 
-    const max = document.querySelector(".max p");
-    max.innerText = `${responseJSON.main.temp_max.toFixed(0)}°`;
+    case 2:
+      return "Tuesday";
 
-    const visibility = document.querySelector(".visibility p");
-    visibility.innerText = `${(responseJSON.visibility / 1000).toFixed(0)}Km`;
+    case 3:
+      return "Wednesday";
 
-    const min = document.querySelector(".min p");
-    min.innerText = `${Math.trunc(responseJSON.main.temp_min)}°`;
-    const humidity = document.querySelector(".humidity p");
-    humidity.innerText = `${responseJSON.main.humidity}%`;
+    case 4:
+      return "Thursday";
 
-    const body = document.querySelector("body");
-    const info = document.querySelectorAll(".info h3");
+    case 5:
+      return "Friday";
 
-    if (responseJSON.weather[0].main === "Clear") {
+    case 6:
+      return "Saturday";
+
+    case 7:
+      return "Sunday";
+  }
+}
+
+function weatherApp(data) {
+  function getWeather(data) {
+    if (data.weather[0].main === "Clear") {
       if (
-        date / 1000 > responseJSON.sys.sunrise &&
-        date / 1000 < responseJSON.sys.sunset
+        dateNow / 1000 > data.sys.sunrise &&
+        dateNow / 1000 < data.sys.sunset
       ) {
         body.classList.add("clear");
       } else {
         body.classList.add("clearNight");
       }
-    } else if (responseJSON.weather[0].main === "Clouds") {
+    }
+
+    if (data.weather[0].main === "Clouds") {
       if (
-        date / 1000 > responseJSON.sys.sunrise &&
-        date / 1000 < responseJSON.sys.sunset
+        dateNow / 1000 > data.sys.sunrise &&
+        dateNow / 1000 < data.sys.sunset
       ) {
         body.classList.add("cloud");
       } else {
@@ -64,12 +83,25 @@ async function getDados(lat, lon) {
         body.classList.add("cloudNight");
         info.forEach((item) => (item.style.color = "white"));
       }
-    } else if (responseJSON.weather[0].main === "Rain") {
-      body.classList.add("rain");
+    }
 
+    if (data.weather[0].main === "Rain") {
+      body.classList.add("rain");
       info.forEach((item) => (item.style.color = "white"));
     }
-  } catch (erro) {
-    console.log(erro);
   }
+  getWeather(data);
+  city.innerText = `${data.name}, ${data.sys.country}`;
+  today.innerText = `${day}
+  ${dateNow.getHours()}:${
+    dateNow.getMinutes() < 10
+      ? "0" + dateNow.getMinutes()
+      : dateNow.getMinutes()
+  }`;
+  degrees.innerText = `${data.main.temp.toFixed(0)}°`;
+  weather.innerText = data.weather[0].description;
+  max.innerText = `${data.main.temp_max.toFixed(0)}°`;
+  visibility.innerText = `${(data.visibility / 1000).toFixed(0)}Km`;
+  min.innerText = `${Math.trunc(data.main.temp_min)}°`;
+  humidity.innerText = `${data.main.humidity}%`;
 }
