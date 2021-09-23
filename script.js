df19b81c9be0568e3fdc01e57c0a0e681f547cb7
Leonardo Burtet <leonardoburtet@outlook.com>
2021-09-23 18:11:14 -0300

    refactor js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const currentDate = new Date();
-const day = dayString(currentDate.getDay());
-const cityH1 = document.querySelector(".city h1");
-const today = document.querySelector(".city h2");
+const currentDay = dayString(currentDate.getDay());
+const nameCity = document.querySelector(".weather__city h1");
+const nameDay = document.querySelector(".weather__city h2");
 const degrees = document.querySelector(".degrees");
 const max = document.querySelector(".max p");
 const min = document.querySelector(".min p");
+const section = document.querySelector("section");
 const body = document.querySelector("body");
 const info = document.querySelectorAll(".info h3");
 const nextDay = document.querySelectorAll(".next-day");
@@ -29,65 +30,65 @@ async function getData(lat, lon) {
     const responseCity = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=decfdd61b5ac2ba68b3b549d378565d0&units=metric`
     );
-    const dataCity = await responseCity.json();
-    const data = await response.json();
+    const currentData = await responseCity.json();
+    const forecastData = await response.json();
 
-    weatherApp(data, dataCity);
+    weatherApp(forecastData, currentData);
   } catch (erro) {
     console.log(erro);
   }
 }
 
-function addNextForecast(data) {
+function addNextForecast(forecastData) {
   let index = 0;
 
   nextDay.forEach((element) => {
     console.log(element.children[2].children[1]);
-    element.children[1].innerText = data.daily[index].weather[0].main;
+    element.children[1].innerText = forecastData.daily[index].weather[0].main;
     element.children[2].children[1].innerText =
-      data.daily[index].temp.max.toFixed(0);
+      forecastData.daily[index].temp.max.toFixed(0);
     element.children[2].children[3].innerText =
-      data.daily[index].temp.min.toFixed(0);
+      forecastData.daily[index].temp.min.toFixed(0);
     index++;
   });
 }
 
-function nextForecast(data) {
+function nextForecast(forecastData) {
   let i = 0;
   const nextDate = currentDate.getDay() + 1;
   if (nextDate <= 4) {
     nextDay.forEach((element) => {
       element.children[0].innerText = dayString(nextDate + i++);
     });
-    addNextForecast(data);
+    addNextForecast(forecastData);
   }
 
   if (nextDate === 5) {
     nextDay[0].children[0].innerText = dayString(5);
     nextDay[1].children[0].innerText = dayString(6);
     nextDay[2].children[0].innerText = dayString(7);
-    addNextForecast(data);
+    addNextForecast(forecastData);
   }
 
   if (nextDate === 6) {
     nextDay[0].children[0].innerText = dayString(6);
     nextDay[1].children[0].innerText = dayString(7);
     nextDay[2].children[0].innerText = dayString(1);
-    addNextForecast(data);
+    addNextForecast(forecastData);
   }
 
   if (nextDate === 7) {
     nextDay[0].children[0].innerText = dayString(7);
     nextDay[1].children[0].innerText = dayString(1);
     nextDay[2].children[0].innerText = dayString(2);
-    addNextForecast(data);
+    addNextForecast(forecastData);
   }
 
   if (nextDate === 8) {
     nextDay[0].children[0].innerText = dayString(1);
     nextDay[1].children[0].innerText = dayString(2);
     nextDay[2].children[0].innerText = dayString(3);
-    addNextForecast(data);
+    addNextForecast(forecastData);
   }
 }
 
@@ -116,47 +117,50 @@ function dayString(day) {
   }
 }
 
-function weatherApp(data, dataCity) {
-  nextForecast(data);
-  function getWeather(data) {
-    if (data.current.weather[0].main === "Clear") {
-      if (
-        currentDate / 1000 > data.current.sunrise &&
-        currentDate / 1000 < data.current.sunset
-      ) {
-        body.classList.add("clear");
-      } else {
-        body.classList.add("clearNight");
-      }
+function getWeather(forecastData) {
+  if (forecastData.current.weather[0].main === "Clear") {
+    if (
+      currentDate / 1000 > forecastData.current.sunrise &&
+      currentDate / 1000 < forecastData.current.sunset
+    ) {
+      body.classList.add("clear");
+    } else {
+      body.classList.add("clearNight");
     }
+  }
 
-    if (data.current.weather[0].main === "Clouds") {
-      if (
-        currentDate / 1000 > data.current.sunrise &&
-        currentDate / 1000 < data.current.sunset
-      ) {
-        body.classList.add("cloud");
-      } else {
-        cityH1.style.color = "white";
-        body.classList.add("cloudNight");
-        info.forEach((item) => (item.style.color = "white"));
-      }
-    }
-
-    if (data.current.weather[0].main === "Rain") {
-      body.classList.add("rain");
+  if (forecastData.current.weather[0].main === "Clouds") {
+    if (
+      currentDate / 1000 > forecastData.current.sunrise &&
+      currentDate / 1000 < forecastData.current.sunset
+    ) {
+      body.classList.add("cloud");
+    } else {
+      nameCity.style.color = "white";
+      body.classList.add("cloudNight");
       info.forEach((item) => (item.style.color = "white"));
     }
   }
-  getWeather(data, dataCity);
-  cityH1.innerText = `${dataCity.name}, ${dataCity.sys.country}`;
-  today.innerText = `${day}
+
+  if (forecastData.current.weather[0].main === "Rain") {
+    body.classList.add("rain");
+    info.forEach((item) => (item.style.color = "white"));
+  }
+}
+
+function weatherApp(forecastData, currentData) {
+  section.style.opacity = "1";
+  section.style.transition = "1s";
+  nextForecast(forecastData);
+  getWeather(forecastData, currentData);
+  nameCity.innerText = `${currentData.name}, ${currentData.sys.country}`;
+  nameDay.innerText = `${currentDay}
   ${currentDate.getHours()}:${
     currentDate.getMinutes() < 10
       ? "0" + currentDate.getMinutes()
       : currentDate.getMinutes()
   }`;
-  degrees.innerText = `${data.current.temp.toFixed(0)}°`;
-  max.innerText = `${data.daily[0].temp.max.toFixed(0)}°`;
-  min.innerText = `${Math.trunc(data.daily[0].temp.min)}°`;
+  degrees.innerText = `${forecastData.current.temp.toFixed(0)}°`;
+  max.innerText = `${forecastData.daily[0].temp.max.toFixed(0)}°`;
+  min.innerText = `${Math.trunc(forecastData.daily[0].temp.min)}°`;
 }
